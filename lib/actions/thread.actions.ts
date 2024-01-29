3275a3b0a62eac25a01bd13161cfc34679a00b86
Akhlakur Rahman <akhlakurrahman@rabby.lan>
2024-01-29 02:04:32 +0100

    thread is populated in the home screen

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -36,3 +36,78 @@ export async function createThread({
 		throw new Error(`Error creating thread. ${error.message}`);
 	}
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+	try {
+		connectToDB();
+
+		// Calculate the number of threads needs to be escaped
+		const skipAmount = (pageNumber - 1) * pageSize;
+
+		const threadsQuery = Thread.find({
+			parentId: { $in: [null, undefined] },
+		})
+			.sort({ createdAt: 'desc' })
+			.skip(skipAmount)
+			.limit(pageSize)
+			.populate({ path: 'author', model: User })
+			.populate({
+				path: 'children',
+				populate: {
+					path: 'author',
+					model: User,
+					select: '_id name parentId image',
+				},
+			});
+
+		const totalThreadsCount = await Thread.countDocuments({
+			parentId: { $in: [null, undefined] },
+		});
+
+		const threads = await threadsQuery.exec();
+
+		const isNext = totalThreadsCount > skipAmount + threads.length;
+
+		return { threads, isNext };
+	} catch (error: any) {
+		throw new Error(`Error while fetching threads ${error.message}`);
+	}
+}
+
+export async function fetchThreadById(id: string) {
+	try {
+		connectToDB();
+
+		//!!! Populate community later
+		const threadById = await Thread.findById(id)
+			.populate({
+				path: 'author',
+				model: User,
+				select: '_id id name image',
+			})
+			.populate({
+				path: 'children',
+				populate: [
+					{
+						path: 'author',
+						model: User,
+						select: '_id id name parentId imgae',
+					},
+					{
+						path: 'children',
+						model: Thread,
+						populate: {
+							path: 'author',
+							model: User,
+							select: '_id id name parentId image',
+						},
+					},
+				],
+			})
+			.exec();
+
+		return threadById;
+	} catch (error: any) {
+		throw new Error(`Error while fetching thread ${error.message}`);
+	}
+}
